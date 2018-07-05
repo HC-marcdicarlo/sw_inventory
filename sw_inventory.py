5310376dd5aacdbb4aeb1f4110471ed46228eeb1
hc-marcdicarlo <marc.dicarlo@harlandclarke.com>
2018-07-05 13:56:10 -0500

    basic functionality for --list

--- sw_inventory.py
+++ sw_inventory.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python
-#sync spacewalk repos and clean up old and orphaned packages
-#repo names can be found in ./repos
 #options can be found in ./options.py
 
 import xmlrpclib
@@ -10,22 +8,67 @@ import ConfigParser
 import optparse
 import sys
 import os
+import json
 
-from options import *
+from opts import *
 from subprocess import *
 from optparse import OptionParser
 
 #open session with spacewalk api
-spacewalk = xmlrpclib.Server("https://%s/rpc/api" % spaceserver, verbose=0)
+spacewalk = xmlrpclib.Server("http://%s/rpc/api" % spaceserver, verbose=0)
 api_token = spacewalk.auth.login(spw_user, spw_pass)
 
-#get the list of systems
-systems = spacewalk.system.listSystems(api_token)
+parser = OptionParser(usage="%prog [options] --list | --host <hostname> | --profile <profile_name>")
+parser.add_option('-o', '--oldlist', dest="oldlist",
+                  default=False, action="store_true",
+                  help="Old list output")
 
-#iterate through system list and print name
-for system in systems:
-    print system['name']
+parser.add_option('-l', '--list', dest="groups",
+                  default=False, action="store_true",
+                  help="System groups")
+
+parser.add_option('-j', dest="js",
+                  default=False, action="store_true",
+                  help="json")
+
+(options, args) = parser.parse_args()
+
+def mk_lst(sys_grp_details):
+    svr_list = []
+    for server in sys_grp_details:
+        svr_list.append(server['name'])
+    return svr_list
+
+
+if options.js:
+    #grpvar = {'grpvar': {'host':['hostone']}}
+    grpvar = {}
+    grp_var = 'grp1'
+    host_list = ['host1', 'host2'] 
+    grpvar[grp_var] = host_list
+    print json.dumps(grpvar)
+
+if options.oldlist:
+    #get the list of systems
+    systems = spacewalk.system.listSystems(api_token)
+    #iterate through system list and print name
+    for system in systems:
+        print system['name']
+
+if options.groups:
+    sys_dict = {}
+    #find all system groups
+    groups = spacewalk.systemgroup.listAllGroups(api_token)
+
+    for group in groups:
+        if group:
+            sys_dict[group['name']] = set()
+        sys_grp_details = spacewalk.systemgroup.listSystemsMinimal(api_token, group['name'])
+        sys_dict[group['name']] = mk_lst(sys_grp_details)
+
+
+    print json.dumps(sys_dict)
+
 
 #logout of api
 spacewalk.auth.logout(api_token)
-
