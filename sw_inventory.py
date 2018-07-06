9ab61ef0a56cb5e39e3e67ef6f5ce93567869ee1
Marc Di Carlo <marcdicarlo@Marcs-MBP.lan>
2018-07-05 23:16:18 -0500

    code clean up and restructure -- add functions mk_list, sys_list and main

--- sw_inventory.py
+++ sw_inventory.py
@@ -1,37 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 #options can be found in ./opts.py
 
 import xmlrpclib
-import httplib
-import datetime
-import ConfigParser
 import optparse
-import sys
-import os
 import json
 
 from opts import *
-from subprocess import *
 from optparse import OptionParser
-
-#open session with spacewalk api
-spacewalk = xmlrpclib.Server("http://%s/rpc/api" % spaceserver, verbose=0)
-api_token = spacewalk.auth.login(spw_user, spw_pass)
-
-parser = OptionParser(usage="%prog [options] --list | --host <hostname> | --profile <profile_name>")
-parser.add_option('-o', '--oldlist', dest="oldlist",
-                  default=False, action="store_true",
-                  help="Old list output")
-
-parser.add_option('-l', '--list', dest="groups",
-                  default=False, action="store_true",
-                  help="System groups")
-
-parser.add_option('-s', '--host', dest="host",
-                  default=False, action="store_true",
-                  help="host vars")
-
-(options, args) = parser.parse_args()
 
 def mk_lst(sys_grp_details):
     svr_list = []
@@ -39,29 +14,58 @@ def mk_lst(sys_grp_details):
         svr_list.append(server['name'])
     return svr_list
 
-if options.host:
-    details = spacewalk.system.getDetails(api_token, 1000010769)
-    print details
-
-if options.oldlist:
-    #get the list of systems
-    systems = spacewalk.system.listSystems(api_token)
-    #iterate through system list and print name
-    for system in systems:
-        print system['name']
-
-if options.groups:
+def sys_list(api_token):
     sys_dict = {}
     #find all system groups
     groups = spacewalk.systemgroup.listAllGroups(api_token)
-
+    #iterate through group
     for group in groups:
+        #if the group detail aren't empty assign the name of the group as a key to sys_dict
         if group:
             sys_dict[group['name']] = set()
+        #get the details of each group
         sys_grp_details = spacewalk.systemgroup.listSystemsMinimal(api_token, group['name'])
+        #append a list of every server in the group to the group key in sys_dict
         sys_dict[group['name']] = mk_lst(sys_grp_details)
-
+    #print sys_dict as json
     print json.dumps(sys_dict)
 
-#logout of api
-spacewalk.auth.logout(api_token)
+def main():
+    #open session with spacewalk api
+    spacewalk = xmlrpclib.Server("http://%s/rpc/api" % spaceserver, verbose=0)
+    api_token = spacewalk.auth.login(spw_user, spw_pass)
+
+    parser = OptionParser(usage="%prog [options] --list | --host <hostname> | --profile <profile_name>")
+    parser.add_option('-o', '--oldlist', dest="oldlist",
+                      default=False, action="store_true",
+                      help="Old list output")
+
+    parser.add_option('-l', '--list', dest="groups",
+                      default=False, action="store_true",
+                      help="System groups")
+
+    parser.add_option('-s', '--host', dest="host",
+                      default=False, action="store_true",
+                      help="host vars")
+
+    (options, args) = parser.parse_args()
+
+    if options.host:
+        details = spacewalk.system.getDetails(api_token, 1000010769)
+        print details
+
+    if options.oldlist:
+        #get the list of systems
+        systems = spacewalk.system.listSystems(api_token)
+        #iterate through system list and print name
+        for system in systems:
+            print system['name']
+
+    if options.groups:
+        sys_list(api_token)
+
+    #logout of api
+    spacewalk.auth.logout(api_token)
+
+if __name__ == "__main__":
+    main()
